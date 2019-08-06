e87ea68212c10bdf33d5a8d89a960101e559e1b2
Zarina <zarina.kurbatova@gmail.com>
2019-08-06 23:52:03 +0300

    Add extractors of PSI elements

---
+++ src/main/java/utils/PsiUtils.java
@@ -0,0 +1,39 @@
+package utils;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectFileIndex;
+import com.intellij.psi.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PsiUtils {
+
+    private static final String FILE_TYPE_NAME = "JAVA";
+
+    public static List<PsiJavaFile> extractFiles(Project project) {
+        final List<PsiJavaFile> javaFiles = new ArrayList<>();
+
+        ProjectFileIndex.SERVICE.getInstance(project).iterateContent(
+                file -> {
+                    PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+                    if (psiFile != null && !psiFile.isDirectory()
+                            && FILE_TYPE_NAME.equals(psiFile.getFileType().getName())) {
+                        javaFiles.add((PsiJavaFile) psiFile);
+                    }
+                    return true;
+                }
+        );
+        return javaFiles;
+    }
+
+    public static List<PsiClass> extractClasses(PsiJavaFile psiFile) {
+        return Arrays.asList(psiFile.getClasses());
+    }
+
+    public static List<PsiMethod> extractMethods(PsiClass psiClass) {
+        return Arrays.asList(psiClass.getAllMethods());
+    }
+
+}
