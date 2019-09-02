a241f90f5337d5909d80bea2a483fb4ee821172f
Zarina <zarina.kurbatova@gmail.com>
2019-09-03 02:03:12 +0300

    Move computations to background mode

--- src/main/java/ui/MoveMethodRefactoringPanel.java
+++ src/main/java/ui/MoveMethodRefactoringPanel.java
@@ -1,8 +1,9 @@
 package ui;
 
-import com.intellij.ide.DataManager;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.ProgressManager;
+import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.SimpleToolWindowPanel;
 
@@ -10,7 +11,6 @@ import gr.uom.java.ast.ASTReader;
 import gr.uom.java.ast.Standalone;
 import gr.uom.java.distance.MoveMethodCandidateRefactoring;
 import gr.uom.java.distance.ProjectInfo;
-import navigation.DataHolder;
 import navigation.Reference;
 
 import java.util.*;
@@ -18,44 +18,34 @@ import java.util.stream.Collectors;
 
 public class MoveMethodRefactoringPanel extends SimpleToolWindowPanel {
     private final MySideBar sideBar;
-    private static MoveMethodRefactoringPanel instance = null;
-    private DumbService dumbService;
     private ProjectInfo projectInfo;
 
-    public MoveMethodRefactoringPanel(boolean vertical, boolean borderless, Project project) {
+    public MoveMethodRefactoringPanel(Project project) {
         super(false, true);
         sideBar = new MySideBar(project);
-        this.dumbService = DumbService.getInstance(project);
         refresh(project);
     }
 
     private void refresh(Project project) {
-        dumbService.runWhenSmart(() -> {
-
-            this.projectInfo = new ProjectInfo(project);
-            new ASTReader(projectInfo);
-            List<MoveMethodCandidateRefactoring> candidates = Standalone.getMoveMethodRefactoringOpportunities(projectInfo);
-            final DataContext context = DataManager.getInstance().getDataContext(this);
-            DataHolder.getInstance().initDataHolder(DataManager.getInstance().getDataContext(this));
-            final List<Reference> references = candidates.stream().filter(Objects::nonNull).limit(50)
-                    .map(x -> new Reference(x.getSourceMethodDeclaration(), x.getTargetClass().getName())).collect(Collectors.toList());
-            sideBar.updateListItems(references);
-            setContent(sideBar.getPanel());
-            instance = this;
-        });
-    }
-
-    public static void refreshList(Project project) {
-        if (instance == null) {
-            instance = new MoveMethodRefactoringPanel(false, false, project);
-        } else
-            instance.refresh(project);
+        this.projectInfo = new ProjectInfo(project);
+        final Task.Backgroundable backgroundable = new Task.Backgroundable(project, "Detecting a Feature Envy smell...", false) {
+            @Override
+            public void run(ProgressIndicator indicator) {
+                ApplicationManager.getApplication().runReadAction(() -> {
+                    new ASTReader(projectInfo);
+                    List<MoveMethodCandidateRefactoring> candidates = Standalone.getMoveMethodRefactoringOpportunities(projectInfo);
+                    final List<Reference> references = candidates.stream().filter(Objects::nonNull)
+                            .map(x -> new Reference(x.getSourceMethodDeclaration(), x.getTargetClass().getName())).collect(Collectors.toList());
+                    sideBar.updateListItems(references);
+                    setContent(sideBar.getPanel());
+                });
+            }
+        };
+        ProgressManager.getInstance().run(backgroundable);
     }
 
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
-        this.dumbService = null;
-        instance = null;
     }
 }
--- src/main/java/ui/RecommendationToolWindowFactory.java
+++ src/main/java/ui/RecommendationToolWindowFactory.java
@@ -5,25 +5,15 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentManager;
-import gr.uom.java.distance.ProjectInfo;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-
 public class RecommendationToolWindowFactory implements ToolWindowFactory {
-    private JPanel jPanel = new JPanel();
-    private JButton button = new JButton();
-    private ProjectInfo projectInfo;
 
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         ContentManager contentManager = toolWindow.getContentManager();
-        Content content = contentManager.getFactory().createContent(new MoveMethodRefactoringPanel(true, true, project), null, false);
+        Content content = contentManager.getFactory().createContent(new MoveMethodRefactoringPanel(project), null, false);
         contentManager.addContent(content);
     }
-
-    private void init(Project project) {
-        this.projectInfo = new ProjectInfo(project);
-    }
-
+    
 }
