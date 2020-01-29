ef5c1b0c4a47a0361d67d36437acfddca4ac18a6
Zarina Kurbatova <zarina.kurbatova@gmail.com>
2020-01-29 19:04:31 +0300

    Review fixes

--- src/main/java/org/jetbrains/research/intellijdeodorant/JDeodorantFacade.java
+++ src/main/java/org/jetbrains/research/intellijdeodorant/JDeodorantFacade.java
@@ -1,19 +1,7 @@
 package org.jetbrains.research.intellijdeodorant;
 
-import com.intellij.ide.errorTreeView.impl.ErrorTreeViewConfiguration;
-import com.intellij.openapi.application.ApplicationManager;
-import com.intellij.openapi.compiler.CompileContext;
-import com.intellij.openapi.compiler.CompileScope;
-import com.intellij.openapi.compiler.CompileStatusNotification;
-import com.intellij.openapi.compiler.CompilerManager;
 import com.intellij.openapi.progress.ProgressIndicator;
-import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiVariable;
-import org.jetbrains.annotations.NotNull;
 import org.jetbrains.research.intellijdeodorant.core.ast.ASTReader;
 import org.jetbrains.research.intellijdeodorant.core.ast.ClassObject;
 import org.jetbrains.research.intellijdeodorant.core.ast.MethodObject;
@@ -21,66 +9,12 @@ import org.jetbrains.research.intellijdeodorant.core.ast.SystemObject;
 import org.jetbrains.research.intellijdeodorant.core.ast.decomposition.cfg.*;
 import org.jetbrains.research.intellijdeodorant.core.distance.*;
 import org.jetbrains.research.intellijdeodorant.ide.refactoring.typeStateChecking.TypeCheckEliminationGroup;
-import org.jetbrains.research.intellijdeodorant.ide.ui.AbstractRefactoringPanel;
 
 import java.util.*;
 
 public class JDeodorantFacade {
-    public static void runAfterCompilationCheck(ProjectInfo projectInfo, Task task) {
-        ApplicationManager.getApplication().invokeLater(() -> {
-            List<PsiClass> classes = projectInfo.getClasses();
 
-            if (!classes.isEmpty()) {
-                VirtualFile[] virtualFiles = classes.stream().map(classObject -> classObject.getContainingFile().getVirtualFile()).toArray(VirtualFile[]::new);
-                Project project = classes.iterator().next().getContainingFile().getProject();
-
-                ErrorTreeViewConfiguration configuration = ErrorTreeViewConfiguration.getInstance(project);
-                boolean wasHideWarnings = configuration.isHideWarnings();
-                boolean wasHideInfoMessages = configuration.isHideInfoMessages();
-                configuration.setHideWarnings(true);
-                configuration.setHideInfoMessages(true);
-
-                CompilerManager compilerManager = CompilerManager.getInstance(project);
-                myCompileStatusNotification callback = new myCompileStatusNotification(task, project, wasHideWarnings, wasHideInfoMessages);
-                CompileScope compileScope = compilerManager.createFilesCompileScope(virtualFiles);
-                compilerManager.make(compileScope, callback);
-            } else {
-                ProgressManager.getInstance().run(task);
-            }
-        });
-    }
-
-    private static class myCompileStatusNotification implements CompileStatusNotification {
-        private Task task;
-        private Project project;
-        boolean wasHideWarnings;
-        boolean wasHideInfoMessages;
-
-        private myCompileStatusNotification(Task task, Project project, boolean wasHideWarnings, boolean wasHideInfoMessages) {
-            this.task = task;
-            this.project = project;
-            this.wasHideWarnings = wasHideWarnings;
-            this.wasHideInfoMessages = wasHideInfoMessages;
-        }
-
-        @Override
-        public void finished(boolean aborted, int errors, int warnings, @NotNull CompileContext compileContext) {
-            ApplicationManager.getApplication().invokeLater(() -> {
-                ErrorTreeViewConfiguration configuration = ErrorTreeViewConfiguration.getInstance(project);
-                configuration.setHideWarnings(wasHideWarnings);
-                configuration.setHideInfoMessages(wasHideInfoMessages);
-            });
-
-            if (errors == 0 && !aborted) {
-                ProgressManager.getInstance().run(task);
-            } else {
-                AbstractRefactoringPanel.showCompilationErrorNotification(task.getProject());
-            }
-        }
-    }
-
-    public static List<MoveMethodCandidateRefactoring> getMoveMethodRefactoringOpportunities(ProjectInfo
-                                                                                                     project, ProgressIndicator indicator) {
+    public static List<MoveMethodCandidateRefactoring> getMoveMethodRefactoringOpportunities(ProjectInfo project, ProgressIndicator indicator) {
         new ASTReader(project, indicator);
         Set<ClassObject> classObjectsToBeExamined = new LinkedHashSet<>(ASTReader.getSystemObject().getClassObjects());
 
@@ -99,8 +33,7 @@ public class JDeodorantFacade {
         return moveMethodCandidateList;
     }
 
-    public static TreeSet<ExtractClassCandidateGroup> getExtractClassRefactoringOpportunities(ProjectInfo
-                                                                                                      project, ProgressIndicator indicator) {
+    public static TreeSet<ExtractClassCandidateGroup> getExtractClassRefactoringOpportunities(ProjectInfo project, ProgressIndicator indicator) {
         new ASTReader(project, indicator);
         SystemObject systemObject = ASTReader.getSystemObject();
         if (systemObject != null) {
@@ -134,8 +67,7 @@ public class JDeodorantFacade {
         }
     }
 
-    public static Set<ASTSliceGroup> getExtractMethodRefactoringOpportunities(ProjectInfo
-                                                                                      project, ProgressIndicator indicator) {
+    public static Set<ASTSliceGroup> getExtractMethodRefactoringOpportunities(ProjectInfo project, ProgressIndicator indicator) {
         new ASTReader(project, indicator);
 
         SystemObject systemObject = ASTReader.getSystemObject();
@@ -156,8 +88,7 @@ public class JDeodorantFacade {
         return extractedSliceGroups;
     }
 
-    private static void processMethod(final Set<ASTSliceGroup> extractedSliceGroups, ClassObject
-            classObject, MethodObject methodObject) {
+    private static void processMethod(final Set<ASTSliceGroup> extractedSliceGroups, ClassObject classObject, MethodObject methodObject) {
         if (methodObject.getMethodBody() != null) {
             CFG cfg = new CFG(methodObject);
             PDG pdg = new PDG(cfg, classObject.getPsiFile(), classObject.getFieldsAccessedInsideMethod(methodObject));
@@ -226,8 +157,7 @@ public class JDeodorantFacade {
         }
     }
 
-    public static Set<TypeCheckEliminationGroup> getTypeCheckEliminationRefactoringOpportunities(ProjectInfo
-                                                                                                         project, ProgressIndicator indicator) {
+    public static Set<TypeCheckEliminationGroup> getTypeCheckEliminationRefactoringOpportunities(ProjectInfo project, ProgressIndicator indicator) {
         new ASTReader(project, indicator);
         SystemObject systemObject = ASTReader.getSystemObject();
 
