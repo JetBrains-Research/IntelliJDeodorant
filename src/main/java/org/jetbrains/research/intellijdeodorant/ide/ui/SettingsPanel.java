f4a53dfb54247e8d60202c8f6262390f733cb93e
Ilya Kireev <ilyakireev53@mail.ru>
2020-03-11 19:10:37 +0300

    Changed realization for testing convenience

--- src/main/java/org/jetbrains/research/intellijdeodorant/ide/ui/SettingsPanel.java
+++ src/main/java/org/jetbrains/research/intellijdeodorant/ide/ui/SettingsPanel.java
@@ -17,6 +17,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.util.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class SettingsPanel extends JPanel {
     private static final String REFACTORING_PLACE_CHOOSE_BOX = "refactorings.place.choose.box";
@@ -50,43 +51,43 @@ public class SettingsPanel extends JPanel {
 
     public static Set<ClassObject> getClassesToFindRefactorings(SystemObject systemObject) {
         String currentComboBoxValue = Objects.requireNonNull(refactoringPlaceChooseBox.getSelectedItem()).toString();
-        switch (currentComboBoxValue) {
-            case "all opened files":
-                return getAllObjectsOpenedInEditor();
-            case "current file":
-                return getCurrentObjectOpenedInEditor();
-            default:
-                return new LinkedHashSet<>(systemObject.getClassObjects());
+        if (currentComboBoxValue.equals("whole project")) {
+            return new LinkedHashSet<>(systemObject.getClassObjects());
+        } else {
+            return getClassesToFindRefactorings(currentComboBoxValue).stream().map(ASTReader::processTypeDeclaration).collect(Collectors.toSet());
         }
-
     }
 
-    private static Set<ClassObject> getAllObjectsOpenedInEditor() {
-        List<ClassObject> classObjectsToBeExamined = new ArrayList<>();
+    public static Set<PsiClass> getClassesToFindRefactorings(String currentComboBoxValue) {
+        if (currentComboBoxValue.equals("all opened files")) {
+            return getAllObjectsOpenedInEditor();
+        } else {
+            return getCurrentObjectOpenedInEditor();
+        }
+    }
+
+    private static Set<PsiClass> getAllObjectsOpenedInEditor() {
+        List<PsiClass> classObjectsToBeExamined = new ArrayList<>();
         VirtualFile[] currentOpenedFiles = FileEditorManager.getInstance(project).getOpenFiles();
         for (VirtualFile virtualFile : currentOpenedFiles) {
             PsiFile psiFile = PsiManager.getInstance(project).findFile(virtualFile);
-            if (psiFile != null) {
-                for (PsiClass psiClass : ((PsiJavaFile) psiFile).getClasses()) {
-                    classObjectsToBeExamined.add(ASTReader.processTypeDeclaration(psiClass));
-                }
+            if (psiFile instanceof PsiJavaFile) {
+                classObjectsToBeExamined.addAll(Arrays.asList(((PsiJavaFile) psiFile).getClasses()));
             }
         }
         return new LinkedHashSet<>(classObjectsToBeExamined);
     }
 
-    private static Set<ClassObject> getCurrentObjectOpenedInEditor() {
-        Set<ClassObject> classObjectsToBeExamined = new LinkedHashSet<>();
+    private static Set<PsiClass> getCurrentObjectOpenedInEditor() {
+        Set<PsiClass> classObjectsToBeExamined = new LinkedHashSet<>();
         FileEditor currentEditor = FileEditorManager.getInstance(project).getSelectedEditor();
         if (currentEditor != null)
         {
             VirtualFile currentFile = currentEditor.getFile();
             if (currentFile != null) {
                 PsiFile psiFile = PsiManager.getInstance(project).findFile(currentFile);
-                if (psiFile != null) {
-                    for (PsiClass psiClass : ((PsiJavaFile) psiFile).getClasses()) {
-                        classObjectsToBeExamined.add(ASTReader.processTypeDeclaration(psiClass));
-                    }
+                if (psiFile instanceof PsiJavaFile) {
+                    classObjectsToBeExamined.addAll(Arrays.asList(((PsiJavaFile) psiFile).getClasses()));
                 }
             }
         }
